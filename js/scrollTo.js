1101ba6fcae9ffc31b7c297d5cc9413b4f3b5af3
Larisa46472 <Angel46472@gmail.com>
2021-12-06 19:24:18 +0300

    добавили плавную прокрутку

--- js/scrollTo.js
+++ js/scrollTo.js
@@ -1,17 +1,20 @@
 const linkDown = document.querySelector('.link--down')
 const secondSection= document.querySelector('.section--about_us')
+const secondSectionY = secondSection.offsetTop
 
-
+let intervalId;
 
 const trackScroll = () => {
-    let scrolled = window.pageYOffset
-
-    window.scrollBy(0, secondSection.offsetTop)
-    
+   
+    window.scrollBy(0, 30)
+    if (window.pageYOffset >= secondSectionY) {
+        clearInterval(intervalId)
+    }
 }
 
 
 linkDown.addEventListener('click', (e) => {
     e.preventDefault()
-    trackScroll()
+    intervalId = setInterval(trackScroll, 10)
+  
 })
